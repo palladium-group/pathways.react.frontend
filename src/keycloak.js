71b889f41579259b7392e0d7d9df0f68043dfefa
felixkithinji <felixkithinji@outlook.com>
2023-11-01 12:05:18 +0300

    changes realm

--- src/keycloak.js
+++ src/keycloak.js
@@ -3,6 +3,6 @@ import Keycloak from "keycloak-js";
 export const kc = new Keycloak({
   url: "https://nextgen-pct-kc.eastus.cloudapp.azure.com/auth",
   realm: "pathways",
-  clientId: "50cef36b-30d6-4cf1-8c7e-313af16a877d",
+  clientId: "pathways-frontend-client",
   onLoad: "login-required",
 });
